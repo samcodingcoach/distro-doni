9ccc8d5078bd6bf925a705b9cd3e6488f11c600c
Samsu Bahri <samsu.bahri@binus.ac.id>
2025-12-19 06:45:58 +0800

    Fix

--- public/index.php
+++ public/index.php
@@ -21,35 +21,7 @@
         scrollbar-width: none;
     }
   </style>
-  <script id="tailwind-config">
-   tailwind.config = {
-      darkMode: "class",
-      theme: {
-        extend: {
-          colors: {
-            "primary": "#d41152",
-            "background-light": "#f8f6f6",
-            "background-dark": "#221016",
-            "foreground-light": "#1b0d12",
-            "foreground-dark": "#f8f6f6",
-            "secondary-light": "#9a4c66",
-            "secondary-dark": "#e0b8c6",
-            "surface-light": "#f3e7eb",
-            "surface-dark": "#3a212a"
-          },
-          fontFamily: {
-            "display": ["Manrope", "sans-serif"]
-          },
-          borderRadius: {
-            "DEFAULT": "0.125rem",
-            "lg": "0.25rem",
-            "xl": "0.5rem",
-            "full": "0.75rem"
-          },
-        },
-      },
-    }
-  </script>
+  <script src="script/tailwind-config.js"></script>
  </head>
  <body class="font-display bg-background-light dark:bg-background-dark text-foreground-light dark:text-foreground-dark">
   <div class="relative flex min-h-screen w-full flex-col">
